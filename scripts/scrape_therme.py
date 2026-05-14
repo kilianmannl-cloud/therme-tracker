8747cedfad6d525563b30ed4f6e961d96aabc5e4
kilianmannl-cloud <kilian.mannl@googlemail.com>
2026-05-14 17:15:26 +0200

    Add real occupancy parser

--- scripts/scrape_therme.py
+++ scripts/scrape_therme.py
@@ -1,4 +1,5 @@
 import csv
+import re
 import requests
 
 from datetime import datetime
@@ -10,9 +11,17 @@ url = "https://www.therme-bad-aibling.de"
 
 response = requests.get(url)
 
+html = response.text
+
 # HTML speichern
 with open("data/page.html", "w", encoding="utf-8") as f:
-    f.write(response.text)
+    f.write(html)
+
+# Prozentwerte finden
+matches = re.findall(r'data-occupied=\"([0-9\\.]+)\"', html)
+
+bad = round(float(matches[0]))
+sauna = round(float(matches[1]))
 
 # Wetterdaten
 weather_url = (
@@ -20,35 +29,3 @@ weather_url = (
     "?latitude=47.86"
     "&longitude=12.01"
     "&current=temperature_2m,weather_code"
-)
-
-weather_data = requests.get(weather_url).json()
-
-temp = weather_data["current"]["temperature_2m"]
-weather_code = weather_data["current"]["weather_code"]
-
-# Noch Testwerte
-bad = 80
-sauna = 95
-
-holiday = False
-bayern_match = False
-germany_match = False
-
-row = [
-    now.isoformat(),
-    bad,
-    sauna,
-    temp,
-    weather_code,
-    holiday,
-    bayern_match,
-    germany_match
-]
-
-with open("data/history.csv", "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(row)
-
-print("HTML gespeichert")
-print("Daten gespeichert")
