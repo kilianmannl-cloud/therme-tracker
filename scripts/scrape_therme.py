2d19cad6ab0daed2748e2c6fb84662d278f1c11b
kilianmannl-cloud <kilian.mannl@googlemail.com>
2026-05-14 16:59:56 +0200

    Update scrape_therme.py

--- scripts/scrape_therme.py
+++ scripts/scrape_therme.py
@@ -1,13 +1,26 @@
 import csv
+import requests
+
 from datetime import datetime
 
 now = datetime.now()
 
+# Wetterdaten von Open-Meteo
+weather_url = (
+    "https://api.open-meteo.com/v1/forecast"
+    "?latitude=47.86"
+    "&longitude=12.01"
+    "&current=temperature_2m,weather_code"
+)
+
+weather_data = requests.get(weather_url).json()
+
+temp = weather_data["current"]["temperature_2m"]
+weather_code = weather_data["current"]["weather_code"]
+
+# Noch Testwerte für Therme
 bad = 80
 sauna = 95
-
-temp = 12
-weather = "rain"
 
 holiday = False
 bayern_match = False
@@ -18,7 +31,7 @@ row = [
     bad,
     sauna,
     temp,
-    weather,
+    weather_code,
     holiday,
     bayern_match,
     germany_match
