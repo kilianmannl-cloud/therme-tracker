b181de445bb9b0d5db062bf6a84c76add407a3c1
kilianmannl-cloud <kilian.mannl@googlemail.com>
2026-05-14 16:57:01 +0200

    Update scrape_therme.py

--- scripts/scrape_therme.py
+++ scripts/scrape_therme.py
@@ -1,1 +1,31 @@
-print("Thermen Tracker läuft")
+import csv
+from datetime import datetime
+
+now = datetime.now()
+
+bad = 80
+sauna = 95
+
+temp = 12
+weather = "rain"
+
+holiday = False
+bayern_match = False
+germany_match = False
+
+row = [
+    now.isoformat(),
+    bad,
+    sauna,
+    temp,
+    weather,
+    holiday,
+    bayern_match,
+    germany_match
+]
+
+with open("data/history.csv", "a", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(row)
+
+print("Daten gespeichert")
